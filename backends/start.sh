173cff922bfb036570dac1ec0a37a3d5e7feead4
GentleDeath1 <achillesdeo1@gmail.com>
2026-04-21 11:13:48 +0100

    Add start.sh to run migrations before server

---
+++ backends/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Running migrations..."
+php artisan migrate --force
+echo "Running seeders..."
+php artisan db:seed --force
+echo "Starting server..."
+php artisan serve --host=0.0.0.0 --port=8080
